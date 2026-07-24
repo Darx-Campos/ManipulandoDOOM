552fa5b35d8f6b18cb5bef39a55962432a874262
maicol hernandez <hernandezmaicolestiven@gmil.com>
2026-07-23 19:33:36 -0500

    Modificando el doom

--- handler.js
+++ handler.js
@@ -1,1 +1,21 @@
-alert("Hola")
+
+let caja = document.getElementById("caja");
+let resultado = document.getElementById("resultado");
+
+caja.addEventListener("keyup", (e) => {
+   // resultado.innerHTML = e.target.value;
+ let palabra = e.target.value;
+ let contarVocales = 0
+ for (let i = 0; i < palabra.length; i++) {
+    switch (palabra[i]){
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+            contarVocales++;
+            break;
+    }
+}
+    resultado.innerHTML = contarVocales;
+});
